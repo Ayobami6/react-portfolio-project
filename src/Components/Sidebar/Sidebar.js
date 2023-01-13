e12a79ca0baf3d3401cfc2bdcffa5b75c918172a
Ayobami <ayobamidele006@gmail.com>
2023-01-13 19:52:21 +0100

    initial commit

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import Home from "../Home/Home";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import SidebarList from "./SidebarList";
+
+const Sidebar = () => {
+  // Sidebar expansion condition expression
+  const [expandSidebar, setExpandSidebar] = useState(true);
+
+  // arrow function for side functionality
+  const handleExpandClick = () => {
+    setExpandSidebar(!expandSidebar);
+  };
+
+  return (
+    <div className="container-fluid sidebar-section">
+      <div className={expandSidebar ? "sidebar-expand sidebar" : "sidebar"}>
+        <div className="icon-for-sidebar-expand">
+          <p onClick={handleExpandClick}>
+            {/* if expand click is true show the left react icon else show right */}
+            {expandSidebar ? (
+              <BsChevronLeft size={30} />
+            ) : (
+              <BsChevronRight size={30} />
+            )}
+          </p>
+        </div>
+        <SidebarList expandSidebar={expandSidebar} />
+      </div>
+
+      <div className="container">
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
